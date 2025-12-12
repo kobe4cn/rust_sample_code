4ee929f62ac9609672850f39cc13b7b16e1843e2
yanglei <5691847+yanglei828818@user.noreply.gitee.com>
2025-12-12 14:15:46 +0800

    feat: for test module

---
+++ adder/target/debug/deps/adder-0bb5ed0f510685d4.d
@@ -0,0 +1,5 @@
+/Users/kevin/dev/rust_bootcamp/boot_camp/adder/target/debug/deps/adder-0bb5ed0f510685d4.d: src/lib.rs
+
+/Users/kevin/dev/rust_bootcamp/boot_camp/adder/target/debug/deps/adder-0bb5ed0f510685d4: src/lib.rs
+
+src/lib.rs:
---
+++ adder/target/debug/deps/adder-e47c0739f346f90a.d
@@ -0,0 +1,7 @@
+/Users/kevin/dev/rust_bootcamp/boot_camp/adder/target/debug/deps/adder-e47c0739f346f90a.d: src/lib.rs
+
+/Users/kevin/dev/rust_bootcamp/boot_camp/adder/target/debug/deps/libadder-e47c0739f346f90a.rlib: src/lib.rs
+
+/Users/kevin/dev/rust_bootcamp/boot_camp/adder/target/debug/deps/libadder-e47c0739f346f90a.rmeta: src/lib.rs
+
+src/lib.rs:
